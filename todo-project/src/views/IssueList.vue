b3d6f4e231cb0be30bdc030a66fff4b123e980ca
Rattuti <i.ishiharar.96296@gmail.com>
2022-08-01 16:27:50 +0900

    second

--- todo-project/src/views/IssueList.vue
+++ todo-project/src/views/IssueList.vue
@@ -3,14 +3,14 @@
     <h1>issueリスト</h1>
     <el-button type="success" @click="getIssues()">issue取得</el-button>
     <el-row :gutter="12">
-      <!-- コード１ -->
-      <el-col :span="12" v-for="issue in issues" :key="issue.id">
+      <!-- コード1 indexも使用できるように追加 -->
+      <el-col :span="12"  v-for="( issue, index ) in issues" :key="issue.id">
         <el-card class="box-card" shadow="hover" style="margin: 5px 0;">
           <el-row :gutter="12">
-             <!-- コード2 -->
             <el-col :span="21">{{ issue.title }}</el-col>
             <el-col :span="3">
-              <el-button type="success" icon="el-icon-check" circle></el-button>
+              <!-- コード2 @click="closeIssue(index)"を追加 -->
+              <el-button @click="closeIssue(index)" type="success" icon="el-icon-check" circle></el-button>
             </el-col>
           </el-row>
         </el-card>
@@ -20,31 +20,46 @@
 </template>
 
 <script>
-import axios from 'axios'; 
+import axios from 'axios';
 
 const client = axios.create({  //--1
-  baseURL: 'https://api.github.com/repos/diveintocode-corp/vue_seriese_api', //--2
+  baseURL: `${process.env.VUE_APP_GITHUB_ENDPOINT}`, //--2
   headers: { //--3
     'Accept': 'application/vnd.github.v3+json',
     'Content-Type':'application/json',
+    'Authorization': `token ${process.env.VUE_APP_GITHUB_TOKEN}`
   },
 })
 
 export default {
   name: 'IssueList',
-  data(){
+  data() {
     return {
       issues: []
     }
   },
-
-methods: {
+  methods: {
     getIssues() {
-        client.get('/issues')
-             .then((res) => {
+      // client定数からget()を呼び出し
+      client.get('/issues') //--4
+        .then((res) => {
           this.issues = res.data;
       })
-    }
+    },
+    closeIssue(index){
+      const target = this.issues[index] //--3
+      client.patch(`/issues/${target.number}`, //--4
+          {
+            state: 'closed' // --5
+          },
+        )
+        .then(() => {
+          this.issues.splice(index, 1) // --6
+      })
+    },
+  },
+  created() {
+    this.getIssues();
   }
 }
 </script>
